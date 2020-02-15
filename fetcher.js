3c800be43bfcfe3b3cf9658b186fcfb68f9f1870
Shuchita <shuchita@live.com>
2020-02-15 02:16:24 +0000

    Page Downloader

--- fetcher.js
+++ fetcher.js
@@ -3,20 +3,18 @@ const fs = require('fs');
 const request = require('request');
 
 request(args[0], (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  // console.log('error:', error); // Print the error if one occurred
+  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   // console.log('body:', body); // Print the HTML for the Google homepage.
 
 
 
   fs.writeFile(args[1], body, function (err) {
     if (err) throw err;
-    console.log(`Downloaded and saved xxxx bytes to ${args[1]}`);
+    console.log(`Downloaded and saved ${fs.statSync("./index.html")['size']} bytes to ${args[1]}`);
   });
 
-
-
   if (response.statusCode !== 200 || error !== null) {
-    // exit?
+    console.log(`Error ${response.statusCode}: ${response}`)
   }
 });
